a7e739031d37e305d088d46baea834b1e592f14c
Auggie <miaugus@gmail.com>
2022-07-04 13:50:46 -0400

    updates to README file and comments

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,23 +1,42 @@
+// sets timer and begins at 75 seconds
 timerEl = document.getElementById("timer");
 var timeLeft = 75;
+
+// selectors
 var startButtonClick = document.querySelector("#startButtonId");
-var randomQuestion;
-var currentQuestion;
 var questionEl = document.getElementById("question");
 var answerEl = document.getElementById("answers");
 var judgmentEl = document.getElementById("judgment");
 var pointsEl = document.getElementById("points")
-var points = 0;
 var highScoresEl = document.getElementById("highScores");
 var scoresEl = document.getElementById("scores");
 var finalScore = document.getElementById("finalScore");
 var submitButtonClick = document.querySelector("#submitButtonId");
 var nomen = document.getElementById("initials");
+
+// global variables 
+var randomQuestion;
+var currentQuestion;
+var points = 0;
+
+// attempting to create leaderboard array for local storage
 var leaderboardArray = [];
 var display = JSON.parse(localStorage.getItem("display")) || [];
 
+// function to initiate game and hide/reveal appropriate elements
+startButtonClick.addEventListener("click", function() {
+    localStorage.removeItem(points);
+    countdown();
+    document.getElementById("startButtonId").style.display = "none";
+    document.getElementById("CQC").style.display = "none";
+    document.getElementById("instructions").style.display = "none";
+    document.getElementById("questionBox").style.visibility = "visible";
+    currentQuestion = 0;
+    randomQuestion = questions.sort(() => Math.random() -0.5);
+    nextQuestion();
+});
 
-
+// function to track timer and trigger end of game
 function countdown() {
     var intervalId = setInterval(function() {
         if (timeLeft > 0) {
@@ -34,22 +53,13 @@ function countdown() {
     }, 1000)
 };
 
-startButtonClick.addEventListener("click", function() {
-    localStorage.removeItem(points);
-    countdown();
-    document.getElementById("startButtonId").style.display = "none";
-    document.getElementById("CQC").style.display = "none";
-    document.getElementById("instructions").style.display = "none";
-    document.getElementById("questionBox").style.visibility = "visible";
-    currentQuestion = 0;
-    randomQuestion = questions.sort(() => Math.random() -0.5);
-    nextQuestion();
-});
 
+// function to select next random question
 function nextQuestion() {
     newQuestion(randomQuestion[currentQuestion]);
 };
 
+// function to generate next question
 function newQuestion(question) {
     randomQuestion = questions.sort(() => Math.random() -0.5);
     questionEl.innerText = question.question;
@@ -65,7 +75,7 @@ function newQuestion(question) {
     }) 
 };
 
-
+// function to check answers
 function answerChosen(e) {
     var chosenButton = e.target;
     if (chosenButton.dataset.correct) {
@@ -80,6 +90,7 @@ function answerChosen(e) {
     nextQuestion();
 };
 
+// function to trigger movement to leaderboard and attempt to log scores
 submitButtonClick.addEventListener("click", function () {
     event.preventDefault();
     var display = {
@@ -92,7 +103,7 @@ submitButtonClick.addEventListener("click", function () {
     window.location = "leaderboard.html";
   });
 
-
+// questions
 var questions =  [
     { 
     question: "Commonly used data types DO NOT include _____",
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Coding-Quiz
+
+## Coding Quiz
+This project utilized HTML, CSS, and Javascript to generate a quiz to test the user's programming knowledge, specifically those in the Georgia Tech bootcamp.<br>
+<br>
+The header displays a link to go to the leaderboard, the score counter, and the timer.<br>
+<br>
+Once a user clicks the "Start Quiz" button, the timer begins counting down from 75 as they are presented with a series of multiple-choice questions.<br>
+<br>
+If a correct answer is selected, they are awarded 1 point (tracked at the top of the screen), "Correct!" is logged at the bottom of the screen, and the next question is presented.<br>
+<br>
+If they make an incorrect selection, 15 seconds are subtracted from the timer, "Wrong!" is logged at the bottom of the screen, and the next question is presented.<br>
+<br>
+When the timer reaches 0, their final score is presented, and they are given the option to enter their initials. After submitting their initials, they are taken to a leaderboard page.<br>
+<br>
+The leaderboard page should track high scores, as well as present two button options: to return to the home screen, or to clear the leaderboard.<br>
+<br>
+The deployed application can be found here: [link to deployed application](https://okapikid.github.io/Coding-Quiz/)
+
+Here is a screenshot of the project: ![Password generator with window prompt visible.](./Assets/Images/screenShot.png)
